90cf46fc555b7241340ba59f7947d826c52ced9b
Doortje Spanjerberg <dodomew@gmail.com>
2017-01-17 12:49:59 +0100

    restructured folder. frontpage now has 3 columns. Linking to pages does not work yet

--- GruntFile.js
+++ GruntFile.js
@@ -1,110 +1,154 @@
-module.exports = function(grunt){
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			compileAndCopy: {
-				options: {
-					style: 'expanded'
+module.exports = function(grunt)
+{
+	//1. Configure my tasks
+	grunt.initConfig
+	(
+		{
+
+			pkg: grunt.file.readJSON('package.json'),
+			//define tasks
+
+			//first task
+			sass: { //task name = parent task
+			compileAndCopy: { //child of parent task.
+				options: { //set options, see https://github.com/gruntjs/grunt-contrib-sass for options
+					style: 'expanded' //output style is : expanded. Other options are nested, compact and compressed. See contrib-sass docs.
 				},
 				files: {
-					'GeometricColor/style.css' : 'scss/style.scss'					
+					/*destination*/ /*'html_dev/css/style.css'*/ 'html_dev/GeometricColor/style.css' : 'html_dev/GeometricColor/scss/style.scss' //source. The task Sass/CompileAndCopy looks at the style.scss and compiles/outputs that to style.css
+					}
 				}
-			}
-		},
-          
-        sync: {
-            theme: {
-                files: [
-                    { cwd: 'GeometricColor/', src: ['**/*.js', '**/*.css', '**/*.php'], dest: 'C:/wamp64/www/doortjespanjerberg/wp-content/themes/GeometricColor'} // makes all src relative to cwd 
-                ],
-                verbose: true, // Default: false 
-                failOnError: true, // Fail the task when copying is not possible. Default: false 
-                updateAndDelete: true // Remove all files from dest that are not found in src. Default: false 
-            },
-            live: {
-                files: [
-                    { cwd: 'GeometricColor/', src: ['**/*.js', '**/*.css', '**/*.php'], dest: 'output'} // makes all src relative to cwd 
-                ],
-                verbose: true, // Default: false 
-                failOnError: true, // Fail the task when copying is not possible. Default: false 
-                updateAndDelete: true // Remove all files from dest that are not found in src. Default: false 
-             }
-            },
-        
-        htmlmin: {                                         // Task
-            dist: {                                      // Target
-                  options: {                                 // Target options
-                  removeComments: true,
-                  collapseWhitespace: true
-                  },
-                  files: {                                   // Dictionary of files
-                    'dist/index.html': '**/*.html'           // 'destination': 'source'
-                      }
-                    },
-                    /*dev: {                                       // Another target
-                      files: {
-                        'dist/index.html': 'src/index.html',
-                        'dist/contact.html': 'src/contact.html'
-                      }
-                    }*/
-                  },
-        
-        cssmin: {
-            minify: {
-                options: {
-                    shorthandCompacting: false,
-                    roundingPrecision: -1
-                    }
-            },
-            target: {
-                files: {
-                  'C:/wamp64/www/doortjespanjerberg/wp-content/themes/GeometricColor/style.css': ['css/style.css']
-                }
-              }
-            },
-                              
-        uglify: {   //uglify is alleen voor js files
-            my_target: {
-                files: [
-                    { expand: true, mangle: false, cwd: 'TangerineZebra/', 
-                    src: ['**/*.js'], 
-                    dest: 'C:/wamp64/www/doortjespanjerberg/wp-content/themes/GeometricColor/js'}
-                    ]
-                }
-            },
-        
-        uncss: {
-          dist: {
-            files: {
-              'cleanCss/style.css': ['css/style.css']
-            }
-          }
-        },
-        
-		watch: {
-			css: {
-				files: 'scss/style.scss',
-				tasks: ['sass']
 			},
-            php: {
-                files: 'GeometricColor/**/*.*',
-                tasks: ['sync:theme']  //Specifically run the 'main' subtask
-            }
-            		
-        }
 
-	});
-    
-    grunt.loadNpmTasks('grunt-sync');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+			sync: //parent task
+			{
+        development: //first child task
+        {
+            files:
+            [
+                {
+                  cwd: 'html_dev/GeometricColor', //I sync files in this location
+                  src: ['**/*.js', '**/*.css', '**/*.php', '**/*.html', '**/*.jpg', //check all files with these extensions
+                  '!**/node_modules/**' /* but exclude txt files */ ],
+                  dest: 'C:/wamp64/www/doortjespanjerberg/wp-content/themes/GeometricColor' //any output I make is put in here
+                } // makes all src relative to Current Working Directory or cwd
+            ],
+
+            verbose: true, // Default: false
+            failOnError: true, // Fail the task when copying is not possible. Default: false
+            updateAndDelete: true // Remove all files from dest that are not found in src. Default: false
+      	},
+
+        live:
+        { //second child task
+            files:
+            [
+                {
+                  cwd: 'html_dev/GeometricColor', //I sync files in this location
+                  src: ['**/*.js', '**/*.css', '**/*.php', '**/*.html', '**/*.jpg'],
+                  dest: 'C:/wamp64/www/doortjespanjerberg/wp-content/themes/GeometricColor'
+              } 	// makes all src relative to cwd
+            ],
+
+            verbose: true, // Default: false
+            failOnError: true, // Fail the task when copying is not possible. Default: false
+            updateAndDelete: true // Remove all files from dest that are not found in src. Default: false
+         }
+      },
+
+			uglify:
+			{
+				options:
+				{
+					mangle: false,
+					preserveComments: false
+				},
+				target:
+				{
+					files:
+					[{
+							expand: true,
+							cwd: 'html_dev/js/',
+							src: ['*.js', '!*.min.js'],
+							dest: 'html_live/js/',
+							ext: '.min.js'
+					}]
+				}
+			},
+
+			htmlmin:
+			{
+				dev:
+				{
+					options:
+					{
+						removeComments: true,
+						collapseWhitespace: true
+					},
+						files:
+						[{
+							expand: true,
+							cwd: 'html_dev',
+							src: ['html_dev/*.html'],
+							dest: 'html_live'
+						}]
+				}
+			},
+
+			cssmin:
+			{
+				target:
+				{
+					files:
+					[{
+						expand: true,
+						cwd: 'html_dev/css',
+						src: ['*.css', '!*.min.css'],
+						dest: 'html_live/css',
+						ext: '.min.css'
+					}]
+				}
+			},
+
+		/*
+		1. watch executes both child tasks css and php.
+		2. css watches the .scss file. php watches everthing in html_dev
+		3. if the file changes, it runs the task 'sass', which is defined further up in this gruntfile
+		4. the sass task compiles the .scss to .css and outputs it to html_dev/style.css
+		5. the watch task looks at the html_dev folder for ANY changes with the 'php' childtask
+		6. when task sass outputs the css file to html_dev, the css file changes, therefore invoking the watch childtask php
+		7. the child task php invokes the task sync:development
+		8. the child task development copies all .js, .css, .php and .html files in html_dev to the destination folder
+		*/
+		watch: //parent task . When I call 'grunt watch' in CMD this is what happens:
+		{
+			css: //child task
+			{
+				files: 'html_dev/GeometricColor/scss/style.scss', //watch this file
+				tasks: ['sass'] //if anything changes in file, run the sass task
+			},
+
+			php: //child task
+			{
+					files: 'html_dev/**/*.*', //check this folder for any file that changes
+					tasks: ['sync:development']  //Specifically run the 'development' subtask of 'sync'
+			}
+		}
+		}
+	);
+
+	//2. Load in my wanted tasks
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-uncss');
-    
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sync');
+
+	//3. Set up my tasks or 'register'
 	grunt.registerTask('default', ['watch']);
-    grunt.registerTask('minify', ['cssmin']);
-    grunt.registerTask('uncss', ['uncss']);
-	//grunt.registerTask('release',['sass', 'sync:live', 'minify', 'upload']);    
+	grunt.registerTask('minify', ['uglify', 'cssmin', 'htmlmin']);
+//(taskName, taskList) . 'default' is run when you do not run specific grunt tasks, so you run in CMD 'grunt'
+	//grunt.registerTask('release',['sass', 'sync:live', 'minify', 'upload']); here the task 'release' runs the subtasks 'sass', 'sync:live', 'minify', 'upload' =>
+	//in CMD write 'grunt release'. Run specific task : 'grunt release:sass'
 }
---
+++ html_live/GeometricColor/category.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+				get_template_part( 'content', get_post_format() );
+
+				endwhile;
+
+				endif;
+			?>
+
+<?php
+
+get_footer();
