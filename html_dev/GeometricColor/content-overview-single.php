fd8bb9bad89a9a497c9fc2024ddee10609887de1
Doortje Spanjerberg <dodomew@gmail.com>
2017-02-07 16:36:14 +0100

    switched sidebar and blogpost positions for better site flow. Tweaked Blogpost css

--- html_dev/GeometricColor/content-overview-single.php
+++ html_dev/GeometricColor/content-overview-single.php
@@ -2,15 +2,18 @@
 
 		<!--<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>-->
 
-	 <?php if(has_post_thumbnail()): ?>
+	 <!--<?php if(has_post_thumbnail()): ?>
 				<figure class="blog-thumbnail">
 						<a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
 								<?php the_post_thumbnail() ?>
 						</a>
 				</figure>
 
-		<?php endif ?>
+		<?php endif ?>-->
 
-	<!--<?php the_excerpt(); ?>-->
+		<main class="post-content-container">
 
+			<?php the_content(); ?>
+
+		</main>
 </div><!-- /.blog-post -->
--- html_dev/GeometricColor/sidebar.php
+++ html_dev/GeometricColor/sidebar.php
@@ -4,10 +4,7 @@
 
     <p>
 
-      n lobortis cursus libero id maximus. Praesent viverra lobortis laoreet. Nullam porta, risus id aliquam sagittis, tellus mi malesuada tortor, ac efficitur est justo in dui. Curabitur sapien ipsum, semper vitae ullamcorper sit amet, malesuada et lorem. Fusce ac urna diam. Aliquam gravida posuere nisi. Aliquam gravida in ipsum mattis porta. Donec diam diam, pretium mattis volutpat quis, condimentum sit amet ipsum. Nullam suscipit rutrum odio, vulputate gravida magna laoreet id.
-Pellentesque dignissim, libero id pretium porttitor, erat leo ultrices ante, vel lacinia magna metus eget ligula. Vestibulum ac justo id turpis hendrerit tempor quis et augue. Nullam consectetur dui lobortis laoreet scelerisque. Nullam vel nibh ante. Aliquam feugiat, mi eu convallis vulputate, nisl mauris commodo lacus, vel vehicula odio leo sit amet turpis. Fusce neque diam, viverra eu ex eget, placerat scelerisque arcu. Maecenas nisi dui, lobortis ultricies mauris id, viverra faucibus lacus. Duis fringilla vitae leo a mollis. Phasellus at sapien a metus lacinia sodales vel sed felis. Fusce efficitur enim nec augue malesuada tempus.
-
-    </p>
+      n lobortis cursus libero id maximus. Praesent viverra lobortis laoreet. Nullam porta, risus id aliquam sagittis, tellus mi malesuada tortor, ac efficitur est justo in dui.
 
   </section>
 
