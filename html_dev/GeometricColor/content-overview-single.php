7f729a09f5939c8332d2f773c2024ca6176e8bfa
Doortje Spanjerberg <dodomew@gmail.com>
2017-02-20 16:25:40 +0100

    tweaked mobile and tablet media queries. Added scss media query mixins for mobile and tablets

--- html_dev/GeometricColor/content-overview-single.php
+++ html_dev/GeometricColor/content-overview-single.php
@@ -1,19 +1,9 @@
 <div class="blog-post">
 
-		<!--<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>-->
-
-	 <!--<?php if(has_post_thumbnail()): ?>
-				<figure class="blog-thumbnail">
-						<a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
-								<?php the_post_thumbnail() ?>
-						</a>
-				</figure>
-
-		<?php endif ?>-->
-
 		<main class="post-content-container">
 
 			<?php the_content(); ?>
 
 		</main>
+		
 </div><!-- /.blog-post -->
--- html_dev/GeometricColor/content-overview.php
+++ html_dev/GeometricColor/content-overview.php
@@ -1,7 +1,5 @@
 <div class="blog-post">
 
-		<!--<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"></a></h2>-->
-
 	 <?php if(has_post_thumbnail()): ?>
 
 		 <a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
